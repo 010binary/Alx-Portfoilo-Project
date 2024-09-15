d4c4a652601b53d9145629bad78c903d7e2ab6b5
010binary <augustine32fg@gmail.com>
2024-09-15 19:20:10 +0100

    added the basic

--- project/main/views.py
+++ project/main/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     return render(request, 'index.html')
 
+
 def register(request):
     return render(request, 'registration.html')
+
+
+def login(request):
+    return render(request, 'login.html')
